17d213eff77d0a6b9201d52fb027453f01753552
AlexHoecht <alex.hoecht@hotmail.com>
2016-11-14 13:29:42 -0500

    Updating to iteration 3

---
+++ src/Error.java
@@ -0,0 +1,43 @@
+
+/**
+ * Used to keep track of an error type, and a packet to generate the error on
+ * @author Andrew
+ *
+ */
+public class Error {
+	
+	private ErrorType errorType; // ErrorType enum
+	private int packetNumber;
+	
+	
+	/**
+	 * Construct an Error with given error type and packet number
+	 * @param errorType
+	 * @param packetNumber
+	 */
+	public Error(ErrorType errorType, int packetNumber)
+	{
+		this.errorType = errorType;
+		this.packetNumber = packetNumber;
+	}
+	
+	/**
+	 * @return the ErrorType
+	 */
+	public ErrorType getErrorType()
+	{
+		return errorType;
+	}
+	
+	
+	/**
+	 * @return the packetNumber for the error to be generated on
+	 */
+	public int getPacketNumber()
+	{
+		return packetNumber;
+	}
+	
+	
+
+}
